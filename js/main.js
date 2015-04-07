51f81db16269ddfb15a56389d46971930ba6765e
Caio Cesar Alves Timoteo <ccesaralvest@gmail.com>
2015-04-07 16:46:04 -0300

    Conteudo principal do site ja com formatacao, aguardando conteudo dos slides do pessoal da sala

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	var opcaoMenu = $('').click();
+
+	// // Funcoes para mostrar as opcoes do menu
+	// $('.navbar-nav').click(function(){
+	// 	$('.section-inicial').fadeOut();
+	// });
+
+	// $('.navbar-brand').click(function(){
+	// 	$('.section-inicial').fadeIn();	
+	// });
+	
+	// // NODE
+	// $('#menu-nodeJs').click(function(){
+	// 	$('#nodeJs').fadeIn();
+	// });
+	// // REDES SOCIAIS
+	// $('#menu-redesSociais').click(function(){
+	// 	$('#redesSociais').fadeIn();
+	// });
+	
+	
+});
