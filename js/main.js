bf07ee1e5aa673084264b21cd23da1bc6c78ffae
Caio Cesar <ccesaralvest@gmail.com>
2015-04-08 01:44:30 -0300

    Nova section (forence) adicionada, menu com jQuery, imagens responsivas, novas referencias para redes sociais

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 $(document).ready(function(){
-	var opcaoMenu = $('').click();
+	var largura = $(window).width();
 
-	// // Funcoes para mostrar as opcoes do menu
-	// $('.navbar-nav').click(function(){
-	// 	$('.section-inicial').fadeOut();
-	// });
-
-	// $('.navbar-brand').click(function(){
-	// 	$('.section-inicial').fadeIn();	
-	// });
-	
-	// // NODE
-	// $('#menu-nodeJs').click(function(){
-	// 	$('#nodeJs').fadeIn();
-	// });
-	// // REDES SOCIAIS
-	// $('#menu-redesSociais').click(function(){
-	// 	$('#redesSociais').fadeIn();
-	// });
-	
+	if (largura > 720){
+		// Mostrar e sumir o menu lateral
+		$('#seta').mouseenter(function(){
+			$('.navbar').fadeIn();
+		});	
+		$('.navbar').mouseleave(function(){
+			$('.navbar').fadeOut();
+		});	
+	}
 	
 });
